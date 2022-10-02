fffe3f54f4170fcfb3161194ccb123fa4a6f7b94
wulongchang <82377340@qq.com>
2022-10-02 18:21:38 +0800

    master2 commit3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git v1");
         System.out.println("git master1");
-        System.out.println("git hot-fix1");
+        System.out.println("git master2");
         System.out.println("git hot-fix2");
         System.out.println("git hot-fix3");
     }
