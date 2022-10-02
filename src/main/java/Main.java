b7a45328ddfedbf58a7cdbdcd2446641797abe0e
82877340 <85475903+82877340@users.noreply.github.com>
2022-10-02 18:36:28 +0800

    Update Main.java
    测试pull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("git v1");
         System.out.println("git hot-fix1");
         System.out.println("git hot-fix33");
+         System.out.println("test pull");
         System.out.println("git hot-fix3");
     }
 }
